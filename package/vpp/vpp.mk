d4cd4827a3dcf193894cb15c7d36142db4753d4f
Ole2mail <ole2mail@mail.com>
2017-05-31 09:06:22 +0100

    fork providing either building VPP as "all-in-one" or building VPP with external DPDK compiled ahead
    in external DPDK case it seems like VPP tends to re-generate DPDK once again as an internal dependency (not resolved yet)
    some patch files were reviewed/appended, patch naming tends to include subfolder name to clarify targeting source

--- package/vpp/vpp.mk
+++ package/vpp/vpp.mk
@@ -62,6 +62,8 @@ HOST_OS = $(shell package/vpp/core.sh)
 #quote-less
 #$(ARCH)
 
+# --- ARCH remapping block
+
 ifeq ($(BR2_ARCH),"arm")
 HOST_VPP_ARCH = arm
 HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-armv7a-$(HOST_OS)app-gcc
@@ -95,29 +97,75 @@ endif
 
 # variables expected inside dpdk or vpp build system
 #CROSS=""
-#PLATFORM=$(HOST_VPP_ARCH)
-#$(HOST_VPP_ARCH)_os=$(HOST_OS)
-#$(HOST_VPP_ARCH)_arch=$(HOST_VPP_ARCH)
-#TARGET_GCC=$(TARGET_CC)
-#DPDK_TARGET=$(HOST_DPDK_CONFIG)
-#DPDK_MACHINE=$(HOST_DPDK_MACHINE)
-#DPDK_CC=$(TARGET_CC)
+#PLATFORM="$(HOST_VPP_ARCH)"
+#$(HOST_VPP_ARCH)_os="$(HOST_OS)"
+#$(HOST_VPP_ARCH)_arch="$(HOST_VPP_ARCH)"
+#$(HOST_VPP_ARCH)_target="$(HOST_VPP_ARCH)-buildroot"
+#TARGET_GCC="$(TARGET_CC)"
+#DPDK_TARGET="$(HOST_DPDK_CONFIG)"
+#DPDK_MACHINE="$(HOST_DPDK_MACHINE)"
+#DPDK_CC="$(TARGET_CC)"
 #EXTRA_CFLAGS="-msse4.2" - will get's enabled by DPDK_DESPERATE_SSSE3=y
+#vpp_uses_external_dpdk="yes"
+#DPDK_INCLUDE="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/include"
+#DPDK_LIB="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/lib"
+#cross_compiling="yes"
+#VPPAPIGEN="output/build/vpp-17.0.4-ioam-anycast/build-root/build-tool-native/tools/vppapigen" 
 
-# configure part
+# --- build host part
+define HOST_VPP_BUILD_CMDS
+	@echo "HOST tools (vppapigen, etc.) BUILD STAGE"
+
+# --- vpp API generation dependency HOST tools only compilation (no need for cross-build configuration)
+	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" CROSS="" PLATFORM="$(HOST_VPP_ARCH)" $(HOST_VPP_ARCH)_os="$(HOST_OS)" $(HOST_VPP_ARCH)_arch="$(HOST_VPP_ARCH)" $(MAKE) v=1 -C $(@D) bootstrap
+endef
+
+# --- configure part
+ifneq ($(BR2_PACKAGE_VPP_DPDK),)
 define VPP_CONFIGURE_CMDS
-	$(TARGET_MAKE_ENV) DPDK_CC=$(TARGET_CC) DPDK_MACHINE=$(HOST_DPDK_MACHINE) DPDK_TARGET=$(HOST_DPDK_CONFIG) CROSS="" PLATFORM=$(HOST_VPP_ARCH) $(HOST_VPP_ARCH)_os=$(HOST_OS) $(HOST_VPP_ARCH)_arch=$(HOST_VPP_ARCH) $(MAKE) v=1 -C $(@D)/dpdk config
-	$(TARGET_MAKE_ENV) TARGET_GCC=$(TARGET_CC) CROSS="" PLATFORM=$(HOST_VPP_ARCH) $(HOST_VPP_ARCH)_os=$(HOST_OS) $(HOST_VPP_ARCH)_arch=$(HOST_VPP_ARCH) $(MAKE) v=1 -C $(@D) bootstrap
-endef
+	@echo "CONFIGURE STAGE" 
 
-# build part
-define VPP_BUILD_CMDS
-	$(TARGET_MAKE_ENV) DPDK_CC=$(TARGET_CC) CROSS="" PLATFORM=$(HOST_VPP_ARCH) $(HOST_VPP_ARCH)_os=$(HOST_OS) $(HOST_VPP_ARCH)_arch=$(HOST_VPP_ARCH) DPDK_TARGET=$(HOST_DPDK_CONFIG) DPDK_MACHINE=$(HOST_DPDK_MACHINE) $(MAKE) v=1 -C $(@D)/dpdk install
-	$(TARGET_MAKE_ENV) TARGET_GCC=$(TARGET_CC) PLATFORM=$(HOST_VPP_ARCH) $(HOST_VPP_ARCH)_os=${HOST_OS} $(HOST_VPP_ARCH)_arch=$(HOST_VPP_ARCH) CROSS="" $(MAKE) v=1 -C $(@D) build \
-		vpp_uses_external_dpdk=yes \
-		  vpp_dpdk_inc_dir=$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/include \
-		    vpp_dpdk_lib_dir=$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/lib
+# --- DPDK configuration for standalone build only
+	$(TARGET_MAKE_ENV) DPDK_CC="$(TARGET_CC)" DPDK_MACHINE="$(HOST_DPDK_MACHINE)" DPDK_TARGET="$(HOST_DPDK_CONFIG)" CROSS="" PLATFORM="$(HOST_VPP_ARCH)" $(HOST_VPP_ARCH)_os="$(HOST_OS)" $(HOST_VPP_ARCH)_arch="$(HOST_VPP_ARCH)" $(MAKE) v=1 -C $(@D)/dpdk config
 endef
+else
+define VPP_CONFIGURE_CMDS
+	@echo "CONFIGURE STAGE" 
+	@echo "NOTHING to CONFIGURE"
+endef
+endif
+
+# --- build part
+ifneq ($(BR2_PACKAGE_VPP_DPDK),)
+define VPP_BUILD_CMDS
+	@echo "BUILD STAGE"
+
+# --- DPDK compilation for standalone build only
+	@echo "DPDK install"
+	$(TARGET_MAKE_ENV) DPDK_CC="$(TARGET_CC)" CROSS="" PLATFORM="$(HOST_VPP_ARCH)" $(HOST_VPP_ARCH)_os="$(HOST_OS)" $(HOST_VPP_ARCH)_arch="$(HOST_VPP_ARCH)" DPDK_TARGET="$(HOST_DPDK_CONFIG)" DPDK_MACHINE="$(HOST_DPDK_MACHINE)" $(MAKE) v=1 -C $(@D)/dpdk install
+
+# --- external DPDK based VPP build process (expected DPDK already configured/compiled/installed with the path provided)
+	@echo "VPP build"
+	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" PLATFORM="$(HOST_VPP_ARCH)" $(HOST_VPP_ARCH)_os="$(HOST_OS)-gnu" $(HOST_VPP_ARCH)_arch="$(HOST_VPP_ARCH)-buildroot" CROSS="" \
+VPPAPIGEN="$(@D)/build-root/build-tool-native/tools/vppapigen" \
+DPDK_INCLUDE="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/include" \
+DPDK_LIB="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/lib" \
+DPDK_CC="$(TARGET_CC)" DPDK_MACHINE="$(HOST_DPDK_MACHINE)" \
+DPDK_TARGET="$(HOST_DPDK_CONFIG)" \
+$(MAKE) v=1 -C $(@D) build 
+endef
+else
+define VPP_BUILD_CMDS
+	@echo "BUILD STAGE"
+
+# --- all-in-one VPP build process (starting with DPDK download/patch/compilation, i.e. require cross-build configuration)
+	@echo "VPP all-in-one build"
+	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" PLATFORM="$(HOST_VPP_ARCH)" $(HOST_VPP_ARCH)_os="$(HOST_OS)-gnu" $(HOST_VPP_ARCH)_arch="$(HOST_VPP_ARCH)-buildroot" CROSS="" \
+VPPAPIGEN="$(@D)/build-root/build-tool-native/tools/vppapigen" \
+DPDK_TARGET="$(HOST_DPDK_CONFIG)" \
+$(MAKE) v=1 -C $(@D) build 
+endef
+endif
 
 # clear part
 define VPP_CLEAN_CMDS
