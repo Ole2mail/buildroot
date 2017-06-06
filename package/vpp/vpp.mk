144ca602957bbb80f938097164872fb4777249d4
Ole2mail <ole2mail@mail.com>
2017-06-06 18:50:44 +0200

    fix aarch64 march identity, architecture naming, defconfig synthesis
    issues:
    - powerpc64 build fails on dpdk compilation
    - aarch64 build fails on vpp installation

--- package/vpp/vpp.mk
+++ package/vpp/vpp.mk
@@ -67,38 +67,41 @@ NR2_ARCH := $(patsubst "%",%,$(BR2_ARCH))
 
 ifeq ($(BR2_ARCH),"arm")
 HOST_VPP_ARCH = arm
-HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-armv7a-$(HOST_OS)app-gcc
+HOST_DPDK_MACHINE = armv7a
+endif
+ifeq ($(BR2_ARCH),"aarch64")
+HOST_VPP_ARCH = arm64
+HOST_DPDK_MACHINE = xgene1
 endif
 ifeq ($(BR2_ARCH),"arm64")
 HOST_VPP_ARCH = arm64
-HOST_DPDK_MACHINE = thunderx
-HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-armv8a-$(HOST_OS)app-gcc
+HOST_DPDK_MACHINE = armv8a
 endif
 ifeq ($(BR2_ARCH),"i486")
 HOST_VPP_ARCH = x86_x32
-HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-native-$(HOST_OS)app-gcc
+HOST_DPDK_MACHINE = native
 endif
 ifeq ($(BR2_ARCH),"i586")
 HOST_VPP_ARCH = x86_x32
-HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-native-$(HOST_OS)app-gcc
+HOST_DPDK_MACHINE = native
 endif
 ifeq ($(BR2_ARCH),"i686")
 HOST_VPP_ARCH = i686
-HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-native-$(HOST_OS)app-gcc
+HOST_DPDK_MACHINE = native
 endif
 ifeq ($(BR2_ARCH),"x86_64")
 HOST_VPP_ARCH = x86_64
 HOST_DPDK_MACHINE = native
-HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-native-$(HOST_OS)app-gcc
 endif
 ifeq ($(BR2_ARCH),"powerpc64")
 HOST_VPP_ARCH = ppc_64
 HOST_DPDK_MACHINE = power8
-HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-power8-$(HOST_OS)app-gcc
 endif
 
+HOST_DPDK_CONFIG = $(HOST_VPP_ARCH)-$(HOST_DPDK_MACHINE)-$(HOST_OS)app-gcc
+
 # variables expected inside dpdk or vpp build system
-#PLATFORM="$(HOST_VPP_ARCH)"
+#PLATFORM="$(NR2_ARCH)"
 #$(BR2_ARCH)_os="$(HOST_OS)"
 #$(BR2_ARCH)_arch="$(NR2_ARCH)"
 #$(BR2_ARCH)_target="$(NR2_ARCH)-buildroot"
@@ -111,8 +114,9 @@ endif
 #DPDK_TUNE="$(HOST_DPDK_MACHINE)"
 #EXTRA_CFLAGS="-msse4.2" - will get's enabled by DPDK_DESPERATE_SSSE3=y
 #vpp_uses_external_dpdk="yes"
-#DPDK_INCLUDE="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/include"
-#DPDK_LIB="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/lib"
+#already "installed" DPDK headers and libraries
+#DPDK_INCLUDE="$(TARGET)/include/dpdk"
+#DPDK_LIB="$(TARGET)/lib"
 #cross_compiling="yes"
 #VPPAPIGEN="output/build/vpp-17.0.4-ioam-anycast/build-root/build-tool-native/tools/vppapigen" 
 
@@ -121,7 +125,7 @@ define HOST_VPP_BUILD_CMDS
 	@echo "HOST tools (vppapigen, etc.) BUILD STAGE"
 
 # --- vpp API generation dependency HOST tools only compilation (no need for cross-build configuration)
-	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" PLATFORM="$(HOST_VPP_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)" $(NR2_ARCH)_arch="$(NR2_ARCH)" $(MAKE) v=1 -C $(@D) bootstrap
+	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" PLATFORM="$(NR2_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)" $(NR2_ARCH)_arch="$(NR2_ARCH)" $(MAKE) v=1 -C $(@D) bootstrap
 endef
 
 # --- configure part
@@ -130,7 +134,7 @@ define VPP_CONFIGURE_CMDS
 	@echo "CONFIGURE STAGE" 
 
 # --- DPDK configuration for standalone build only
-	$(TARGET_MAKE_ENV) DPDK_TUNE="$(HOST_DPDK_MACHINE)" DPDK_MACHINE="$(HOST_DPDK_MACHINE)" DPDK_TARGET="$(HOST_DPDK_CONFIG)" CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" PLATFORM="$(HOST_VPP_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)" $(NR2_ARCH)_arch="$(NR2_ARCH)" $(MAKE) v=1 -C $(@D)/dpdk config
+	$(TARGET_MAKE_ENV) DPDK_TUNE="$(HOST_DPDK_MACHINE)" DPDK_MACHINE="$(HOST_DPDK_MACHINE)" DPDK_TARGET="$(HOST_DPDK_CONFIG)" CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" PLATFORM="$(NR2_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)" $(NR2_ARCH)_arch="$(NR2_ARCH)" $(MAKE) v=1 -C $(@D)/dpdk config
 endef
 else
 define VPP_CONFIGURE_CMDS
@@ -146,15 +150,15 @@ define VPP_BUILD_CMDS
 
 # --- DPDK compilation for standalone build only
 	@echo "DPDK install"
-	$(TARGET_MAKE_ENV) CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" PLATFORM="$(HOST_VPP_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)" $(NR2_ARCH)_arch="$(NR2_ARCH)" DPDK_TUNE="$(HOST_DPDK_MACHINE)" DPDK_TARGET="$(HOST_DPDK_CONFIG)" DPDK_MACHINE="$(HOST_DPDK_MACHINE)" $(MAKE) v=1 -C $(@D)/dpdk install
+	$(TARGET_MAKE_ENV) CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" PLATFORM="$(NR2_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)" $(NR2_ARCH)_arch="$(NR2_ARCH)" DPDK_TUNE="$(HOST_DPDK_MACHINE)" DPDK_TARGET="$(HOST_DPDK_CONFIG)" DPDK_MACHINE="$(HOST_DPDK_MACHINE)" $(MAKE) v=1 -C $(@D)/dpdk install
 
 # --- external DPDK based VPP build process (expected DPDK already configured/compiled/installed with the path provided)
 	@echo "VPP build"
-	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" PLATFORM="$(HOST_VPP_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)-gnu" $(NR2_ARCH)_arch="$(NR2_ARCH)-buildroot" \
+	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" PLATFORM="$(NR2_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)-gnu" $(NR2_ARCH)_arch="$(NR2_ARCH)-buildroot" \
 		CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" \
 VPPAPIGEN="$(@D)/build-root/build-tool-native/tools/vppapigen" \
-DPDK_INCLUDE="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/include" \
-DPDK_LIB="$(@D)/dpdk/_build/dpdk-17.02/$(HOST_DPDK_CONFIG)/lib" \
+DPDK_INCLUDE="$(TARGET)/include/dpdk" \
+DPDK_LIB="$(TARGET)/lib" \
 DPDK_MACHINE="$(HOST_DPDK_MACHINE)" \
 DPDK_TARGET="$(HOST_DPDK_CONFIG)" \
 DPDK_TUNE="$(HOST_DPDK_MACHINE)" \
@@ -166,7 +170,7 @@ define VPP_BUILD_CMDS
 
 # --- all-in-one VPP build process (starting with DPDK download/patch/compilation, i.e. require cross-build configuration)
 	@echo "VPP all-in-one build"
-	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" PLATFORM="$(HOST_VPP_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)-gnu" $(NR2_ARCH)_arch="$(NR2_ARCH)-buildroot" \
+	$(TARGET_MAKE_ENV) TARGET_GCC="$(TARGET_CC)" PLATFORM="$(NR2_ARCH)" $(NR2_ARCH)_os="$(HOST_OS)-gnu" $(NR2_ARCH)_arch="$(NR2_ARCH)-buildroot" \
 		CROSS="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-" \
 CONFIG_RTE_TOOLCHAIN="$(NR2_ARCH)-buildroot-$(HOST_OS)-gnu-gcc" \
 VPPAPIGEN="$(@D)/build-root/build-tool-native/tools/vppapigen" \
